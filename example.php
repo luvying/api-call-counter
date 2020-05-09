22f14903312e4ffaa33d1aa3138f2881996dc243
Ying <46587259+cxying@users.noreply.github.com>
2020-05-10 00:15:26 +0800

    Update example.php

--- example.php
+++ example.php
@@ -3,7 +3,7 @@
 // require 会生成致命错误（E_COMPILE_ERROR）并停止脚本
 // include 只生成警告（E_WARNING），并且脚本会继续
 // 定义实际运行统计程序的路径,绝对路径
-$path = '/www/open/acgimg/pv.php';  
+$path = '/www/xxx/xxx/pv.php';  
 include('runpv.php');
 // ---------- pv统计 ----------
 
